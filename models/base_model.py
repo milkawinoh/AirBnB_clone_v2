56a6d519479c64f364ed4d98165ed27ca19cd773
milkawinoh <milkawinoh@gmail.com>
2023-11-22 11:09:00 +0300

    console.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,24 +8,26 @@ class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        if not kwargs or 'id' not in kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if isinstance(kwargs['updated_at'], str):
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if isinstance(kwargs['created_at'], str):
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if '__class__' in kwargs:
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
-
+            
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        cls = type(self).__name__
+        obj_id = getattr(self, 'id', None)
+        return '[{}] ({}) {}'.format(cls, obj_id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
