01d97420064857ceb9ce57e145e50268eb94339d
milkawinoh <milkawinoh@gmail.com>
2024-01-20 19:06:55 -0800

    task

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -36,28 +36,4 @@ exec { '/usr/bin/env apt -y update' : }
 -> exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
--> file { '/var/www':
-  ensure => 'directory'
-}
--> file { '/var/www/html':
-  ensure => 'directory'
-}
--> file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => "<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    <p>Nginx server test</p>
-  </body>
-</html>"
-}
-exec { 'nginx_conf':
-  environment => ['data=\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'],
-  command     => 'sed -i "39i $data" /etc/nginx/sites-enabled/default',
-  path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin'
-}
--> service { 'nginx':
-  ensure => running,
-}
+                                           
